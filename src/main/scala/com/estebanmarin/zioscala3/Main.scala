a8e2fa53135d8ad3c79ea64bba0447e1d0fd319f
esteban <esteban.marin@adidas.com>
2021-09-09 12:16:37 -0500

    working trace

--- src/main/scala/com/estebanmarin/zioscala3/Main.scala
+++ src/main/scala/com/estebanmarin/zioscala3/Main.scala
@@ -3,12 +3,21 @@ package zioscala3
 
 import zio.*
 import zio.console.*
-object Main extends App:
-  override def run(args: List[String]): zio.URIO[zio.ZEnv, zio.ExitCode] =
-    (for
-      _ <- console.putStrLn("─" * 100)
-      _ <- console.putStrLn("What's your name")
-      name <- console.getStrLn
+import java.io.IOException
+object Main extends scala.App:
+  val trace = s"[${scala.Console.BLUE}Result of the program =>${scala.Console.RESET}]"
+  val program: ZIO[Console, IOException, Unit] =
+    for
+      _ <- console.putStrLn("─" * 100).debug(trace)
+      _ <- console.putStrLn("What's your name").debug(trace)
+      // name <- console.getStrLn
+      name <- ZIO.succeed("Esteban").debug(trace)
+      // name <- ZIO.fail[IOException](error = )
       _ <- console.putStrLn(s"Hello $name")
       _ <- console.putStrLn("─" * 100)
-    yield ()).exitCode
+    yield ()
+
+  Runtime.default.unsafeRun(program.repeatN(n = 0))
+
+// override def run(args: List[String]): zio.URIO[zio.ZEnv, zio.ExitCode] =
+//   program.exitCode
