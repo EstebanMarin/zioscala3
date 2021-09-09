092d5020d9cb26f3fd93e766ad7287cd9c75d9aa
esteban <esteban.marin@adidas.com>
2021-09-09 15:55:58 -0500

    ready to change

--- src/main/scala/com/estebanmarin/zioscala3/Main.scala
+++ src/main/scala/com/estebanmarin/zioscala3/Main.scala
@@ -3,21 +3,21 @@ package zioscala3
 
 import zio.*
 import zio.console.*
-import java.io.IOException
 object Main extends scala.App:
   val trace = s"[${scala.Console.BLUE}Result of the program =>${scala.Console.RESET}]"
-  val program: ZIO[Console, IOException, Unit] =
+  lazy val program =
     for
       _ <- console.putStrLn("─" * 100).debug(trace)
       _ <- console.putStrLn("What's your name").debug(trace)
       // name <- console.getStrLn
-      name <- ZIO.succeed("Esteban").debug(trace)
-      // name <- ZIO.fail[IOException](error = )
+      name: String <- ZIO.succeed("Esteban").debug(trace)
+      // // name <- ZIO.fail(error = sys.error("boom"))
+      // _ <- ZIO.effect(effect = throw java.lang.RuntimeException("Boom"))
       _ <- console.putStrLn(s"Hello $name")
       _ <- console.putStrLn("─" * 100)
     yield ()
 
-  Runtime.default.unsafeRun(program.repeatN(n = 0))
+  Runtime.default.unsafeRunSync(program.repeatN(n = 0))
 
 // override def run(args: List[String]): zio.URIO[zio.ZEnv, zio.ExitCode] =
 //   program.exitCode
---
+++ src/main/scala/com/estebanmarin/zioscala3/ourzio.scala
@@ -0,0 +1,35 @@
+package com.estebanmarin.zioscala3
+
+object ourzio:
+  final case class ZIO[A](thunk: () => A):
+    def flatMap[B](azb: A => ZIO[B]): ZIO[B] =
+      ZIO.succeed {
+        val a: A = thunk()
+        val zb: ZIO[B] = azb(a)
+        val b: B = zb.thunk()
+        b
+      }
+
+    def map[B](ab: A => B): ZIO[B] =
+      ZIO.succeed {
+        val a = thunk()
+        val b = ab(a)
+        b
+      }
+
+  end ZIO
+
+  object ZIO:
+    def succeed[A](a: => A): ZIO[A] =
+      ZIO(() => a)
+
+  object console:
+    def putStrLn(line: => String) =
+      ZIO.succeed(println(line))
+    def getStrLn =
+      ZIO.succeed(scala.io.StdIn.readLine)
+
+  object Runtime:
+    object default:
+      def unsafeRunSync[A](zio: => ZIO[A]) =
+        zio.thunk()
