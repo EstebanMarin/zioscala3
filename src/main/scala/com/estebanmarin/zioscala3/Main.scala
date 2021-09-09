8eb212491ed76dc06d8f5906cb4fb70337d27c5b
esteban <esteban.marin@adidas.com>
2021-09-09 16:39:28 -0500

    feature, implement ZIO.fail

--- src/main/scala/com/estebanmarin/zioscala3/Main.scala
+++ src/main/scala/com/estebanmarin/zioscala3/Main.scala
@@ -3,16 +3,15 @@ package zioscala3
 
 import zio.*
 // import ourzio.*
+
 object Main extends scala.App:
   val trace = s"[${scala.Console.BLUE}Result of the program =>${scala.Console.RESET}]"
   lazy val program =
     for
       _ <- console.putStrLn("─" * 100)
       _ <- console.putStrLn("What's your name")
-      // name <- console.getStrLn
       name: String <- ZIO.succeed("Esteban")
-      // // name <- ZIO.fail(error = sys.error("boom"))
-      // _ <- ZIO.effect(effect = throw java.lang.RuntimeException("Boom"))
+      _ <- ZIO.fail(error = "boom")
       _ <- console.putStrLn(s"Hello $name")
       _ <- console.putStrLn("─" * 100)
     yield ()
