b929da9b277a23fef7b50df171996032af9cb9f5
esteban <esteban.marin@adidas.com>
2021-09-13 10:36:21 -0500

    created into our ZIO the error channel

--- src/main/scala/com/estebanmarin/zioscala3/Main.scala
+++ src/main/scala/com/estebanmarin/zioscala3/Main.scala
@@ -4,71 +4,71 @@ package zioscala3
 // import zio.*
 // import ourzio.*
 
-// object Main extends scala.App:
-//   lazy val program =
-//     for
-//       _ <- console.putStrLn("─" * 100)
-//       _ <- console.putStrLn("What's your name")
-//       name: String <- ZIO.succeed("Esteban")
-//       // _ <- ZIO
-//       // .effect(throw runtimeexception("boom"))
-//       // .maperror(_.getmessage)
-//       // .catchAll(h = _ => ZIO.succeed(println("Solved the error")))
-//       _ <- console.putStrLn(s"Hello $name")
-//       _ <- console.putStrLn("─" * 100)
-//     yield ()
-
-trait BusinessLogic:
-  def doesGoogleHaveEvenAmountOfPicturesOf(topic: String): Boolean
-
-trait Google:
-  def countPicturesOf(topic: String): Int
-
-object BusinessLogic:
-  lazy val live: Google => BusinessLogic =
-    fromFunction(make)
-
-  def make(google: Google): BusinessLogic =
-    new:
-      override def doesGoogleHaveEvenAmountOfPicturesOf(topic: String): Boolean =
-        google.countPicturesOf(topic) % 2 == 0
-
-object GoogleImp:
-  lazy val live: Any => Google =
-    succeed(make)
-  lazy val make: Google =
-    new:
-      override def countPicturesOf(topic: String): Int =
-        if topic == "cats" then 1337 else 1338
-
-object DependecyGraph:
-  lazy val live: Any => BusinessLogic =
-    val google = GoogleImp.live.provide(()).apply(())
-    val businessLogic = BusinessLogic.live.provide(google)
-    businessLogic
-
-  lazy val make: BusinessLogic =
-    val google: Google = GoogleImp.make
-    //classical dependency injection
-    val businessLogic = BusinessLogic.make(google)
-    businessLogic
-
 object Main extends scala.App:
-  lazy val businessLogic = DependecyGraph.live.apply(())
+  lazy val program =
+    for
+      _ <- console.putStrLn("─" * 100)
+      _ <- console.putStrLn("What's your name")
+      name: String <- ZIO.succeed("Esteban")
+      // _ <- ZIO
+      // .effect(throw runtimeexception("boom"))
+      // .maperror(_.getmessage)
+      // .catchAll(h = _ => ZIO.succeed(println("Solved the error")))
+      _ <- console.putStrLn(s"Hello $name")
+      _ <- console.putStrLn("─" * 100)
+    yield ()
 
-  lazy val bl = DependecyGraph.live
+// trait BusinessLogic:
+//   def doesGoogleHaveEvenAmountOfPicturesOf(topic: String): Boolean
 
-  println("-" * 50)
-  println(businessLogic.doesGoogleHaveEvenAmountOfPicturesOf("cats"))
-  println(businessLogic.doesGoogleHaveEvenAmountOfPicturesOf("dogs"))
-  println("=" * 50)
+// trait Google:
+//   def countPicturesOf(topic: String): Int
 
-extension [R, A](run: R => A)
-  def provide(r: => R): Any => A =
-    _ => run(r)
+// object BusinessLogic:
+//   lazy val live: Google => BusinessLogic =
+//     fromFunction(make)
 
-def succeed[A](a: => A): Any => A =
-  _ => a
+//   def make(google: Google): BusinessLogic =
+//     new:
+//       override def doesGoogleHaveEvenAmountOfPicturesOf(topic: String): Boolean =
+//         google.countPicturesOf(topic) % 2 == 0
 
-def fromFunction[R, A](run: R => A): R => A =
-  r => run(r)
+// object GoogleImp:
+//   lazy val live: Any => Google =
+//     succeed(make)
+//   lazy val make: Google =
+//     new:
+//       override def countPicturesOf(topic: String): Int =
+//         if topic == "cats" then 1337 else 1338
+
+// object DependecyGraph:
+//   lazy val live: Any => BusinessLogic =
+//     val google = GoogleImp.live.provide(()).apply(())
+//     val businessLogic = BusinessLogic.live.provide(google)
+//     businessLogic
+
+//   lazy val make: BusinessLogic =
+//     val google: Google = GoogleImp.make
+//     //classical dependency injection
+//     val businessLogic = BusinessLogic.make(google)
+//     businessLogic
+
+// object Main extends scala.App:
+//   lazy val businessLogic = DependecyGraph.live.apply(())
+
+//   lazy val bl = DependecyGraph.live
+
+//   println("-" * 50)
+//   println(businessLogic.doesGoogleHaveEvenAmountOfPicturesOf("cats"))
+//   println(businessLogic.doesGoogleHaveEvenAmountOfPicturesOf("dogs"))
+//   println("=" * 50)
+
+// extension [R, A](run: R => A)
+//   def provide(r: => R): Any => A =
+//     _ => run(r)
+
+// def succeed[A](a: => A): Any => A =
+//   _ => a
+
+// def fromFunction[R, A](run: R => A): R => A =
+//   r => run(r)
--- src/main/scala/com/estebanmarin/zioscala3/ourzio.scala
+++ src/main/scala/com/estebanmarin/zioscala3/ourzio.scala
@@ -1,21 +1,21 @@
 package com.estebanmarin
 package zioscala3
 
-final class ZIO[+E, +A](val thunk: () => Either[E, A]):
-  def flatMap[E1 >: E, B](azb: A => ZIO[E1, B]): ZIO[E1, B] =
-    ZIO { () =>
-      val errorOrA = thunk()
+final class ZIO[R, +E, +A](val run: R => Either[E, A]):
+  def flatMap[E1 >: E, B](azb: A => ZIO[R, E1, B]): ZIO[R, E1, B] =
+    ZIO { r =>
+      val errorOrA = run(r)
       // val zErrorb = errorOrA.fold(fa = e => ZIO.fail(e), fb = a => azb(a))
       val zErrorb = errorOrA match
         case Right(a) => azb(a)
         case Left(e) => ZIO.fail(e)
-      val errorOrB = zErrorb.thunk()
+      val errorOrB = zErrorb.run(r)
       errorOrB
     }
 
-  def map[B](ab: A => B): ZIO[E, B] =
-    ZIO { () =>
-      val errorOrA = thunk()
+  def map[B](ab: A => B): ZIO[R, E, B] =
+    ZIO { r =>
+      val errorOrA = run(r)
       val errorOrB = errorOrA match
         case Right(a) => Right(ab(a))
         case Left(e) => Left(e)
@@ -23,20 +23,20 @@ final class ZIO[+E, +A](val thunk: () => Either[E, A]):
       errorOrB
     }
 
-  def catchAll[E2, A1 >: A](h: E => ZIO[E2, A1]): ZIO[E2, A1] =
-    ZIO { () =>
-      val errorOrA = thunk()
+  def catchAll[E2, A1 >: A](h: E => ZIO[R, E2, A1]): ZIO[R, E2, A1] =
+    ZIO { r =>
+      val errorOrA = run(r)
       // val zErrorb = errorOrA.fold(fa = h, fb = ZIO.succeed)
       val zErrorb = errorOrA match
         case Right(a) => ZIO.succeed(a)
         case Left(e) => h(e)
-      val errorOrB = zErrorb.thunk()
+      val errorOrB = zErrorb.run(r)
       errorOrB
     }
 
-  def mapError[E2](h: E => E2): ZIO[E2, A] =
-    ZIO { () =>
-      val errorOrA = thunk()
+  def mapError[E2](h: E => E2): ZIO[R, E2, A] =
+    ZIO { r =>
+      val errorOrA = run(r)
       val errorOrB = errorOrA match
         case Right(a) => Right(a)
         case Left(e) => Left(h(e))
@@ -47,14 +47,14 @@ final class ZIO[+E, +A](val thunk: () => Either[E, A]):
 end ZIO
 
 object ZIO:
-  def succeed[A](a: => A): ZIO[Nothing, A] =
-    ZIO(() => Right(a))
+  def succeed[A](a: => A): ZIO[Any, Nothing, A] =
+    ZIO(r => Right(a))
 
-  def fail[E](e: => E): ZIO[E, Nothing] =
-    ZIO(() => Left(e))
+  def fail[E](e: => E): ZIO[Any, E, Nothing] =
+    ZIO(r => Left(e))
 
-  def effect[A](a: => A): ZIO[Throwable, A] =
-    ZIO { () =>
+  def effect[A](a: => A): ZIO[Any, Throwable, A] =
+    ZIO { r =>
       try Right(a)
       catch case ex: Throwable => Left(ex)
     }
@@ -62,10 +62,10 @@ object ZIO:
 object console:
   def putStrLn(line: => String) =
     ZIO.succeed(println(line))
-  def getStrLn =
+  lazy val getStrLn =
     ZIO.succeed(scala.io.StdIn.readLine)
 
 object Runtime:
   object default:
-    def unsafeRunSync[E, A](zio: => ZIO[E, A]) =
-      zio.thunk()
+    def unsafeRunSync[E, A](zio: => ZIO[Any, E, A]): Either[E, A] =
+      zio.run(())
